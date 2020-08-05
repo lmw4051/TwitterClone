e2c2983d0304f5847ad575709f4b385bef027771
David <dvdlee41@gmail.com>
2020-08-05 15:03:13 +0800

    Set MainTabController as rootViewController

--- TwitterClone/Controller/MainTabController.swift
+++ TwitterClone/Controller/MainTabController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainTabController.swift
 //  TwitterClone
 //
 //  Created by David on 2020/8/5.
@@ -8,13 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class MainTabController: UITabBarController {
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    view.backgroundColor = .systemPink
   }
-
-
 }
-
