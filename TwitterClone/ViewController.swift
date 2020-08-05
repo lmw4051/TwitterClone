1593c3dd1502da4e0b8b42ed24e5ce60edeffca2
David <dvdlee41@gmail.com>
2020-08-05 14:10:40 +0800

    Creates the license file for this repo

---
+++ TwitterClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitterClone
+//
+//  Created by David on 2020/8/5.
+//  Copyright © 2020 David. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
