25f47dc12d725e9ba4367ee701cea46ca7403102
David <dvdlee41@gmail.com>
2020-08-06 10:56:48 +0800

    Adding logoImageView, email related views, password related views, loginButton and attributed button.

--- TwitterClone/Controller/Authentication/LoginController.swift
+++ TwitterClone/Controller/Authentication/LoginController.swift
@@ -10,6 +10,48 @@ import UIKit
 
 class LoginController: UIViewController {
   // MARK: - Properties
+  private let logoImageView: UIImageView = {
+    let iv = UIImageView()
+    iv.contentMode = .scaleAspectFit
+    iv.clipsToBounds = true
+    iv.image = #imageLiteral(resourceName: "TwitterLogo")
+    return iv
+  }()
+  
+  private lazy var emailContainerView: UIView = {
+    let image = #imageLiteral(resourceName: "ic_mail_outline_white_2x-1")
+    let view = Utilities().inputContainerView(withImage: image, textField: emailTextField)
+    return view
+  }()
+  
+  private lazy var passwordContainerView: UIView = {
+    let image = #imageLiteral(resourceName: "ic_lock_outline_white_2x")
+    let view = Utilities().inputContainerView(withImage: image, textField: passwordTextField)
+    return view
+  }()
+  
+  private let emailTextField: UITextField = {
+    let tf = Utilities().textField(withPlaceholder: "Email")
+    return tf
+  }()
+  
+  private let passwordTextField: UITextField = {
+    let tf = Utilities().textField(withPlaceholder: "password")
+    tf.isSecureTextEntry = true
+    return tf
+  }()
+  
+  private let loginButton: UIButton = {
+    let button = Utilities().loginAndSignUpButton("Log In")
+    button.addTarget(self, action: #selector(handleLogin), for: .touchUpInside)
+    return button
+  }()
+  
+  private let dontHaveAccountButton: UIButton = {
+    let button = Utilities().attributeButton("Don't have a account", " Sign Up")
+    button.addTarget(self, action: #selector(handleShowSignUp), for: .touchUpInside)
+    return button
+  }()
   
   // MARK: - Lifecycle
   override func viewDidLoad() {
@@ -18,11 +60,43 @@ class LoginController: UIViewController {
   }
   
   // MARK: - Selectors
+  @objc func handleLogin() {
+    print("handleLogin")
+  }
+  
+  @objc func handleShowSignUp() {
+    print("handleShowSignUp")
+  }
   
   // MARK: - Helpers
   func configureUI() {
     view.backgroundColor = .twitterBlue
     navigationController?.navigationBar.barStyle = .black
-    navigationController?.navigationBar.isHidden = true        
+    navigationController?.navigationBar.isHidden = true
+    
+    view.addSubview(logoImageView)
+    logoImageView.centerX(inView: view, topAnchor: view.safeAreaLayoutGuide.topAnchor)
+    logoImageView.setDimensions(width: 150, height: 150)
+    
+    let stack = UIStackView(arrangedSubviews: [emailContainerView,
+                                               passwordContainerView,
+                                               loginButton])
+    stack.axis = .vertical
+    stack.spacing = 20
+    stack.distribution = .fillEqually
+    
+    view.addSubview(stack)
+    stack.anchor(top: logoImageView.bottomAnchor,
+                 left: view.leftAnchor,
+                 right: view.rightAnchor,
+                 paddingLeft: 16,
+                 paddingRight: 16)
+    
+    view.addSubview(dontHaveAccountButton)
+    dontHaveAccountButton.anchor(left: view.leftAnchor,
+                                 bottom: view.safeAreaLayoutGuide.bottomAnchor,
+                                 right: view.rightAnchor,
+                                 paddingLeft: 40,
+                                 paddingRight: 40)
   }
 }
