630a446fa73c6025c92efcac0b355027c94763fc
David <dvdlee41@gmail.com>
2020-09-23 23:59:09 +0800

    Adding Tweet model and function for fetching tweet with Firebase in TweetService and FeedController

---
+++ TwitterClone/Controller/Tweet.swift
@@ -0,0 +1,31 @@
+//
+//  Tweet.swift
+//  TwitterClone
+//
+//  Created by David on 2020/9/23.
+//  Copyright © 2020 David. All rights reserved.
+//
+
+import Foundation
+
+struct Tweet {
+  let tweetID: String
+  let caption: String
+  let uid: String
+  let likes: Int
+  let reTweetCount: Int
+  var timeStamp: Date!
+  
+  init(tweetID: String, dictionary: [String: Any]) {
+    self.tweetID = tweetID
+    
+    self.caption = dictionary["caption"] as? String ?? ""
+    self.uid = dictionary["uid"] as? String ?? ""
+    self.likes = dictionary["likes"] as? Int ?? 0
+    self.reTweetCount = dictionary["reTweetCount"] as? Int ?? 0
+  
+    if let timeStamp = dictionary["timeStamp"] as? Double {
+      self.timeStamp = Date(timeIntervalSince1970: timeStamp)
+    }
+  }  
+}
