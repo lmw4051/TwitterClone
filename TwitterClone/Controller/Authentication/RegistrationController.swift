309308e95dd7099043d3d2a0c41517463cdeb527
David <dvdlee41@gmail.com>
2020-09-23 14:25:14 +0800

    Adding StackView including email/password/full name/user name fields

--- TwitterClone/Controller/Authentication/RegistrationController.swift
+++ TwitterClone/Controller/Authentication/RegistrationController.swift
@@ -18,7 +18,51 @@ class RegistrationController: UIViewController {
     return button
   }()
   
+  private lazy var emailContainerView: UIView = {
+    let image = #imageLiteral(resourceName: "ic_mail_outline_white_2x-1")
+    let view = Utilities().inputContainerView(withImage: image, textField: emailTextField)
+    return view
+  }()
   
+  private lazy var passwordContainerView: UIView = {
+    let image = #imageLiteral(resourceName: "ic_lock_outline_white_2x")
+    let view = Utilities().inputContainerView(withImage: image, textField: passwordTextField)
+    return view
+  }()
+  
+  private lazy var fullNameContainerView: UIView = {
+    let image = #imageLiteral(resourceName: "ic_person_outline_white_2x")
+    let view = Utilities().inputContainerView(withImage: image, textField: fullNameTextField)
+    return view
+  }()
+  
+  private lazy var userNameContainerView: UIView = {
+    let image = #imageLiteral(resourceName: "ic_person_outline_white_2x")
+    let view = Utilities().inputContainerView(withImage: image, textField: userNameTextField)
+    return view
+  }()
+  
+  private let emailTextField: UITextField = {
+    let tf = Utilities().textField(withPlaceholder: "Email")
+    return tf
+  }()
+  
+  private let passwordTextField: UITextField = {
+    let tf = Utilities().textField(withPlaceholder: "password")
+    tf.isSecureTextEntry = true
+    return tf
+  }()
+  
+  private let fullNameTextField: UITextField = {
+    let tf = Utilities().textField(withPlaceholder: "Full Name")
+    return tf
+  }()
+  
+  private let userNameTextField: UITextField = {
+    let tf = Utilities().textField(withPlaceholder: "User Name")
+    tf.isSecureTextEntry = true
+    return tf
+  }()
   
   private let alreadyHaveAccountButton: UIButton = {
     let button = Utilities().attributeButton("Already have an account", " Log In")
@@ -48,7 +92,21 @@ class RegistrationController: UIViewController {
     plusPhotoButton.centerX(inView: view, topAnchor: view.safeAreaLayoutGuide.topAnchor)
     plusPhotoButton.setDimensions(width: 128, height: 128)
     
+    let stack = UIStackView(arrangedSubviews: [emailContainerView,
+                                               passwordContainerView,
+                                               fullNameContainerView,
+                                               userNameContainerView])
+    stack.axis = .vertical
+    stack.spacing = 20
+    stack.distribution = .fillEqually
     
+    view.addSubview(stack)
+    stack.anchor(top: plusPhotoButton.bottomAnchor,
+                 left: view.leftAnchor,
+                 right: view.rightAnchor,
+                 paddingTop: 32,
+                 paddingLeft: 32,
+                 paddingRight: 32)
     
     view.addSubview(alreadyHaveAccountButton)
     alreadyHaveAccountButton.anchor(left: view.leftAnchor,
