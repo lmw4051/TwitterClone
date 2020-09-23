7c54a8a6e252a46fb379c573f97b86fcbf5cee7f
David <dvdlee41@gmail.com>
2020-09-23 17:26:39 +0800

    Adding check whether use is logged in or not

--- TwitterClone/Controller/MainTabController.swift
+++ TwitterClone/Controller/MainTabController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class MainTabController: UITabBarController {
   // MARK: - Properties
@@ -22,8 +23,32 @@ class MainTabController: UITabBarController {
   // MARK: - Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
-    configureUI()
-    configureViewControllers()
+    
+//    logUserOut()
+    view.backgroundColor = .twitterBlue
+    authenticateUserAndConfigureUI()
+  }
+  
+  // MARK: - API
+  func authenticateUserAndConfigureUI() {
+    if Auth.auth().currentUser == nil {
+      DispatchQueue.main.async {
+        let nav = UINavigationController(rootViewController: LoginController())
+        nav.modalPresentationStyle = .fullScreen
+        self.present(nav, animated: true, completion: nil)
+      }
+    } else {
+      configureUI()
+      configureViewControllers()
+    }
+  }
+  
+  func logUserOut() {
+    do {
+      try Auth.auth().signOut()
+    } catch let error {
+      print("DEBUG: Failed to sign out with error \(error.localizedDescription)")
+    }
   }
   
   // MARK: - Selectors
