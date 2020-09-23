e05f15e0f8cfe418fa0b62ea10fcc0e7d036ada5
David <dvdlee41@gmail.com>
2020-09-23 13:38:39 +0800

    Adding Registration Controller

---
+++ TwitterClone/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,24 @@
+//
+//  RegistrationController.swift
+//  TwitterClone
+//
+//  Created by David on 2020/9/23.
+//  Copyright © 2020 David. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+  // MARK: - Properties
+  
+  // MARK: - Lifecycle
+  override func viewDidLoad() {
+    configureUI()
+  }
+  // MARK: - Selectors
+  
+  // MARK: - Helpers
+  func configureUI() {
+    view.backgroundColor = .twitterBlue
+  }
+}
