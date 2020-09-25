01448460d21996910fd89e185c221c52c8f8a8f4
David <dvdlee41@gmail.com>
2020-09-25 15:24:55 +0800

    Cretaing followers & following string and actionButtonTitle data setting and check in ProfileHeaderViewModel

--- TwitterClone/Controller/ProfileController.swift
+++ TwitterClone/Controller/ProfileController.swift
@@ -13,11 +13,21 @@ private let headerIdentifier = "ProfileHeader"
 
 class ProfileController: UICollectionViewController {
   // MARK: - Properties
+  private let user: User
   
   // MARK: - Lifecycle
+  init(user: User) {
+    self.user = user
+    super.init(collectionViewLayout: UICollectionViewFlowLayout())
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    configureCollectionView()
+    configureCollectionView()        
   }
   
   override func viewWillAppear(_ animated: Bool) {
@@ -51,6 +61,7 @@ extension ProfileController {
 extension ProfileController {
   override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
     let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerIdentifier, for: indexPath) as! ProfileHeader
+    header.user = user
     return header
   }
 }
--- TwitterClone/Controller/ProfileHeader.swift
+++ TwitterClone/Controller/ProfileHeader.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ProfileHeader: UICollectionReusableView {
   // MARK: - Properties
+  var user: User? {
+    didSet {
+      configure()
+    }
+  }
+  
   private let filterBar = ProfileFilterView()
   
   private lazy var containerView: UIView = {
@@ -81,6 +87,24 @@ class ProfileHeader: UICollectionReusableView {
     return view
   }()
   
+  private let followingLabel: UILabel = {
+    let label = UILabel()
+    label.text = "0 Following"
+    let followTap = UITapGestureRecognizer(target: self, action: #selector(handleFollowersTapped))
+    label.isUserInteractionEnabled = true
+    label.addGestureRecognizer(followTap)
+    return label
+  }()
+  
+  private let followersLabel: UILabel = {
+    let label = UILabel()
+    label.text = "2 Followers"
+    let followTap = UITapGestureRecognizer(target: self, action: #selector(handleFollowingTapped))
+    label.isUserInteractionEnabled = true
+    label.addGestureRecognizer(followTap)
+    return label
+  }()
+  
   // MARK: - Lifecycle
   override init(frame: CGRect) {
     super.init(frame: frame)
@@ -124,6 +148,17 @@ class ProfileHeader: UICollectionReusableView {
                             paddingLeft: 12,
                             paddingRight: 12)
     
+    let followStack = UIStackView(arrangedSubviews: [followingLabel, followersLabel])
+    followStack.axis = .horizontal
+    followStack.spacing = 8
+    followStack.distribution = .fillEqually
+    
+    addSubview(followStack)
+    followStack.anchor(top: userDetailsStack.bottomAnchor,
+                       left: leftAnchor,
+                       paddingTop: 8,
+                       paddingLeft: 12)
+    
     addSubview(filterBar)
     filterBar.anchor(left: leftAnchor,
                      bottom: bottomAnchor,
@@ -148,6 +183,26 @@ class ProfileHeader: UICollectionReusableView {
   
   @objc func handleEditProfileFollow() {
     print("handleEditProfileFollow")
+  }
+  
+  @objc func handleFollowersTapped() {
+    
+  }
+  
+  @objc func handleFollowingTapped() {
+    
+  }
+  
+  // MARK: - Helpers
+  func configure() {
+    guard let user = user else { return }
+    let viewModel = ProfileHeaderViewModel(user: user)
+    
+    profileImageView.sd_setImage(with: user.profileImageUrl)
+    
+    editProfileFollowButton.setTitle(viewModel.actionButtonTitle, for: .normal)
+    followingLabel.attributedText = viewModel.followingString
+    followersLabel.attributedText = viewModel.followersString
   }
 }
 
