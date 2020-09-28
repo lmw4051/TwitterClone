85d786b20f889a767f11ab458a12c9be248b59fb
David <dvdlee41@gmail.com>
2020-09-28 14:58:12 +0800

    Adding function for clicking image and cell of NotificationController will go to user profile page

--- TwitterClone/Controller/NotificationsController.swift
+++ TwitterClone/Controller/NotificationsController.swift
@@ -44,6 +44,7 @@ class NotificationsController: UITableViewController {
   }
 }
 
+// MARK: - UITableViewDataSource
 extension NotificationsController {
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return notifications.count
@@ -52,6 +53,30 @@ extension NotificationsController {
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! NotificationCell
     cell.notification = notifications[indexPath.row]
+    cell.delegate = self
     return cell
+  }
+}
+
+// MARK: - UITableViewDelegate
+extension NotificationsController {
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    let notification = notifications[indexPath.row]
+    
+    guard let tweetID = notification.tweetID else { return }
+    TweetService.shared.fetchTweet(withTweetID: tweetID) { tweet in
+      let controller = TweetController(tweet: tweet)
+      self.navigationController?.pushViewController(controller, animated: true)
+    }
+  }
+}
+
+// MARK: - NotificationCellDelegate
+extension NotificationsController: NotificationCellDelegate {
+  func didTapProfileImage(_ cell: NotificationCell) {
+    guard let user = cell.notification?.user else { return }
+    
+    let controller = ProfileController(user: user)
+    navigationController?.pushViewController(controller, animated: true)
   }
 }
