e0d69915f40f45209afa37d62a0b3ad4270bde0e
David <dvdlee41@gmail.com>
2020-09-28 15:53:19 +0800

    Adding refreshControl for fetching notification

--- TwitterClone/Controller/NotificationsController.swift
+++ TwitterClone/Controller/NotificationsController.swift
@@ -26,18 +26,28 @@ class NotificationsController: UITableViewController {
     fetchNotifications()
   }
   
+  // MARK: - Selectors
+  @objc func handleRefresh() {
+    fetchNotifications()
+  }
+  
   // MARK: - API
   func fetchNotifications() {
+    refreshControl?.beginRefreshing()
     NotificationService.shared.fetchNotifications { notifications in
+      self.refreshControl?.endRefreshing()
       self.notifications = notifications
-      
-      for (index, notification) in notifications.enumerated() {
-        if case .follow = notification.type {
-          let user = notification.user
-          
-          UserService.shared.checkIfUserIsFollowed(uid: user.uid) { isFollowed in
-            self.notifications[index].user.isFollowed = isFollowed
-          }
+      self.checkIfUserIsFollowed(notifications: notifications)
+    }
+  }
+  
+  func checkIfUserIsFollowed(notifications: [Notification]) {
+    for (index, notification) in notifications.enumerated() {
+      if case .follow = notification.type {
+        let user = notification.user
+        
+        UserService.shared.checkIfUserIsFollowed(uid: user.uid) { isFollowed in
+          self.notifications[index].user.isFollowed = isFollowed
         }
       }
     }
@@ -51,6 +61,10 @@ class NotificationsController: UITableViewController {
     tableView.register(NotificationCell.self, forCellReuseIdentifier: reuseIdentifier)
     tableView.rowHeight = 60
     tableView.separatorStyle = .none
+    
+    let refreshControl = UIRefreshControl()
+    tableView.refreshControl = refreshControl
+    refreshControl.addTarget(self, action: #selector(handleRefresh), for: .valueChanged)
   }
 }
 
@@ -91,6 +105,18 @@ extension NotificationsController: NotificationCellDelegate {
   }
   
   func didTapFollow(_ cell: NotificationCell) {
-    print("didTapFollow")
+    guard let user = cell.notification?.user else { return }
+    
+    print("User is followed \(user.isFollowed)")
+    
+    if user.isFollowed {
+      UserService.shared.unfollowerUser(uid: user.uid) { (err, ref) in
+        cell.notification?.user.isFollowed = false
+      }
+    } else {
+      UserService.shared.followUser(uid: user.uid) { (err, ref) in
+        cell.notification?.user.isFollowed = true
+      }
+    }
   }
 }
